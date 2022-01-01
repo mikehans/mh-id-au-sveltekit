9560960c2dfcaf18555a62504217bcee28281f53
Mike Hansford <mjhansford@gmail.com>
2022-01-01 16:03:45 +1100

    Refactor header to component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { mainMenu } from '../stores';
+
+	console.log(`mainMenu`, $mainMenu);
+</script>
+
+<header>
+    <nav>
+        {#each $mainMenu as item}
+            <a href={item.slug}>{item.label}</a> &nbsp;&nbsp;
+        {/each}
+    </nav>
+</header>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,7 @@
 <script lang="ts">
-	import {testMe, mainMenu} from '../stores';
-
-	console.log(`mainMenu`, $mainMenu)
+	import Header from "$lib/Header.svelte";	
 </script>
 
-<nav>
-	{#each $mainMenu as item}
-		<a href={item.slug}>{item.label}</a> &nbsp;&nbsp;
-	{/each}
-</nav>
-
-
+<Header />
 
 <slot />
