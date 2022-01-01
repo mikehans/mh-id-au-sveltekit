5fe0b72d3dd8222e1d4364882db722728b9a0226
Mike Hansford <mjhansford@gmail.com>
2022-01-01 19:09:10 +1100

    Syntax fix for netlify adapter usage

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import {adapter as netlifyAdapter} from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,8 +9,8 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: netlifyAdapter({
-			
+		adapter: adapter({
+
 		}),
 
 		// hydrate the <div id="svelte"> element in src/app.html
