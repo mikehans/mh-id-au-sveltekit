0d0fc2879086d9b7a5149e7c710d714bdcc59f17
Mike Hansford <mjhansford@gmail.com>
2022-01-01 15:57:17 +1100

    update param type

--- src/markdownToHtml.ts
+++ src/markdownToHtml.ts
@@ -2,6 +2,7 @@ import { remark } from 'remark';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
 
-export async function markdownToHtml(markdownContent: any) : Promise<any>{
-    return await remark().use(remarkRehype).use(rehypeStringify).process(markdownContent);
+export async function markdownToHtml(markdownContent: string): Promise<any> {
+    // console.log(`Inside markdownToHtml: markdownContent`, markdownContent);
+	return await remark().use(remarkRehype).use(rehypeStringify).process(markdownContent);
 }
