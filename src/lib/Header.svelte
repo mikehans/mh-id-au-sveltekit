fe218097559a6f1c93fa0ec47eede3a71541a9a6
Mike Hansford <mjhansford@gmail.com>
2022-01-01 18:19:56 +1100

    Initial styling for main menu

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,24 +1,41 @@
 <script lang="ts">
 	import { mainMenu } from '../stores';
+	import { page } from '$app/stores';
 </script>
 
 <header>
-    <nav>
-        <ul>
-        {#each $mainMenu as item}
-           <li><a href={item.slug}>{item.label}</a> </li>
-        {/each}
-    </ul>
-    </nav>
+	<nav>
+		<ul>
+			{#each $mainMenu as item}
+				<li class:active = {$page.path === item.slug}><a href={item.slug}>{item.label}</a></li>
+			{/each}
+		</ul>
+	</nav>
 </header>
 
 <style>
-    ul{
-        padding-left: 0;
+	ul {
+		padding-left: 0;
+	}
+	li {
+		list-style: none;
+		display: inline;
+		padding-right: 10px; 
+	}
+
+    li > a {
+        text-decoration: none;  
     }
-    ul > li {
-        list-style: none;
-        display: inline;
-        padding-right: 10px;
+
+    li > a:hover {
+        background-color: bisque;
+    }
+
+    li > a:visited {
+        text-decoration: none;
+    }
+
+    .active {
+        text-decoration: underline;
     }
 </style>
